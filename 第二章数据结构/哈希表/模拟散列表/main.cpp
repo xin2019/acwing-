69a5f8897fe3e05efe26826438a3a072802e803e
阿尔法 <1720878303@qq.com>
2023-03-05 11:54:40 +0800

    模拟散列表

--- 第二章数据结构/哈希表/模拟散列表/main.cpp
+++ 第二章数据结构/哈希表/模拟散列表/main.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
+#include<iostream>
 #include<cstring>
 using namespace std;
 const int N=100003;
+const int null=0x3f3f3f3f;
+int h[N],idx,e[N],ne[N];
 
 //拉链法
-int idx,e[N],ne[N],h[N];
-
-void insertX(int x){
+void insertLink(int x){
     int k=(x%N+N)%N;
-   // int head=h[k];
     e[idx]=x;
     ne[idx]=h[k];
     h[k]=idx++;
 }
-bool findX(int x){
+bool findLinkX(int x){
     int k=(x%N+N)%N;
     for(int i=h[k];i!=-1;i=ne[i]){
         if(e[i]==x)
@@ -21,60 +20,44 @@ bool findX(int x){
     }
     return false;
 }
-
-//开放地址法
-const int null =0x3f3f3f3f;
-
-void insertX2(int x){
-    int k=(x%N+N)%N;
-    while(h[k]!=null){
-        k++;
-        if(k==N)
-            k=0;
-    }
-    h[k]=x;
-}
-bool findX2(int x){
-    int k=(x%N+N)%N;
-    while(h[k]!=null){
-        if(h[k]==x)
-            return true;
-        k++;
-    }
-    return false;
-}
-
-int main1()
-{
-    int n,x;
-    memset(h,-1,sizeof(h));
-    cin>>n;
-    char op;
-    while(n--){
-        cin>>op>>x;
-        if(op=='I'){
-            insertX(x);
-        }else{
-            cout<<(findX(x)==true?"Yes":"No")<<endl;
-        }
-    }
-    return 0;
-}
-
-int main(){
-
+int main1(){
     int n;
-    memset(h,0x3f,sizeof(h));
     cin>>n;
+    memset(h,-1,sizeof(h));
     while(n--){
         char op;
         int x;
         cin>>op>>x;
-        if(op=='I')
-        {
-            insertX2(x);
+        if(op=='I'){
+            insertLink(x);
         }else{
-            cout<<(findX2(x)==true?"Yes":"No")<<endl;
+            cout<<(findLinkX(x)?"Yes":"No")<<endl;
+        }
+    }
+    return 0;
+}
+//开放定址法
+int findK(int x){
+    int k=(x%N+N)%N;
+    while(h[k]!=null&&h[k]!=x){
+        k++;
+        if(k==N)
+            k=0;
+    }
+    return k;
+}
+int main(){
+    int n;
+    cin>>n;
+    memset(h,0x3f,sizeof(h));
+    while(n--){
+        char op;
+        int x;
+        cin>>op>>x;
+        if(op=='I'){
+            h[findK(x)]=x;
+        }else{
+            cout<<(h[findK(x)]==null?"No":"Yes")<<endl;
         }
     }
     return 0;
