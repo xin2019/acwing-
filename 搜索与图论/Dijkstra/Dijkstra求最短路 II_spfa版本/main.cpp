df3065dbc38daaff960a26f3f4544ac4a49bb5f2
阿尔法 <1720878303@qq.com>
2023-03-25 20:59:44 +0800

    Dijkstra求最短路 II_spfa版本

--- 搜索与图论/Dijkstra/Dijkstra求最短路 II_spfa版本/main.cpp
+++ 搜索与图论/Dijkstra/Dijkstra求最短路 II_spfa版本/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
-
+#include<cstring>
+#include<queue>
 using namespace std;
-
+const int N=1e5+10;
+int h[N],ne[N],e[N],idx,dist[N],w[N];
+int n,m,a,b,c;
+bool st[N];
+void add(int a,int b,int c){
+    w[idx]=c;
+    e[idx]=b;
+    ne[idx]=h[a];
+    h[a]=idx++;
+}
+int spfa(){
+    memset(dist,0x3f,sizeof dist);
+    queue<int> que;
+    que.push(1);
+    dist[1]=0;
+    st[1]=true;
+    while(que.size()){
+        int t=que.front();
+        que.pop();
+        st[t]=false;
+        for(int i=h[t];~i;i=ne[i]){
+            int j=e[i];
+            if(dist[j]>dist[t]+w[i]){
+                dist[j]=dist[t]+w[i];
+                if(!st[j]){
+                    st[j]=true;
+                    que.push(j);
+                }
+            }
+        }
+    }
+    if(dist[n] == 0x3f3f3f3f)
+        return -1;
+    return dist[n];
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    cin>>n>>m;
+    memset(h,-1,sizeof h);
+    for(int i=0;i<m;i++){
+        cin>>a>>b>>c;
+        add(a,b,c);
+    }
+    int t = spfa();
+    if(t == -1)
+        cout<<"impossible"<<endl;
+    else
+        cout<<t<<endl;
     return 0;
 }
