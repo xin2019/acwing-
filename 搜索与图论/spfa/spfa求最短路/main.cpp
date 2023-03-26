22c3084a16c6849236122aebc086d642ad95f27c
阿尔法 <1720878303@qq.com>
2023-03-26 16:05:51 +0800

    spfa求最短路

--- 搜索与图论/spfa/spfa求最短路/main.cpp
+++ 搜索与图论/spfa/spfa求最短路/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include<iostream>
 #include<cstring>
 #include<queue>
 using namespace std;
 const int N=1e5+10;
-int h[N],dist[N],idx,ne[N],e[N],w[N];
 int n,m,a,c,b;
+int h[N],e[N],ne[N],idx,w[N],dist[N];
 bool st[N];
 void add(int a,int b,int c){
     w[idx]=c;
@@ -15,39 +15,41 @@ void add(int a,int b,int c){
 int spfa(){
     memset(dist,0x3f,sizeof dist);
     dist[1]=0;
-    st[1]=true;
     queue<int> que;
     que.push(1);
+    st[1]=true;
     while(que.size()){
-        int t = que.front();
+        int t=que.front();
         que.pop();
-        for(int i = h[t]; ~i ;i = ne[i]){
-            int j = e[i];
-            if(dist[j] > dist[t] + w[i]){
-                dist[j] = dist[t] + w[i];
+        st[t]=false;
+        for(int i=h[t];~i;i=ne[i]){
+            int j=e[i];
+            if(dist[j]>dist[t]+w[i]){
+                dist[j]=dist[t]+w[i];
                 if(!st[j]){
-                    st[j] = true;
+                    st[j]=true;
                     que.push(j);
                 }
             }
         }
     }
-    if(dist[n] == 0x3f3f3f3f)
+    if(dist[n]>0x3f3f3f3f/2)
         return -1;
     return dist[n];
 }
-int main()
-{
-    cin>>n>>m;
+int main(){
     memset(h,-1,sizeof h);
-    for(int i = 0; i < m; i ++){
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
         cin>>a>>b>>c;
         add(a,b,c);
     }
     int t=spfa();
-    if(t == -1)
+    if(t==-1){
         cout<<"impossible"<<endl;
-    else
+    }
+    else{
         cout<<t<<endl;
+    }
     return 0;
 }
