6ac75e172642e680bd3b287e1aad89a2401d402e
阿尔法 <1720878303@qq.com>
2023-04-21 14:49:32 +0800

    埃氏筛法筛质数

---
+++ 第四章数学知识/质数/筛质数/埃氏筛法/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+const int N =1e6 + 10;
+int primes[N], cnt;
+bool st[N];
+void get_primes(int n) {
+    for(int i = 2; i <= n; i ++ ) {
+        if(!st[i]){
+            primes[cnt++] = i;
+            for(int j = i + i; j <= n; j += i ) {
+                st[j] = true;
+            }
+        }
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    get_primes(n);
+    cout << cnt << endl;
+    return 0;
+}
